2ee6f42e90d31eed743dc92213897e5ed6d497c9
ChenYikunReal <50993163+ChenYikunReal@users.noreply.github.com>
2020-07-24 13:13:17 +0800

    将JDBC升级为MyBatis

--- src/main/java/com/train/service/impl/StudentInfoManagementImpl.java
+++ src/main/java/com/train/service/impl/StudentInfoManagementImpl.java
@@ -1,16 +1,36 @@
 package com.train.service.impl;
 
 import com.train.dao.StudentInfoDao;
+import com.train.dao.StudentInfoMapper;
 import com.train.pojo.StudentInfo;
+import com.train.pojo.StudentInfoExample;
 import com.train.service.StudentInfoManagement;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 public class StudentInfoManagementImpl implements StudentInfoManagement {
 
     private StudentInfoDao dao = StudentInfoDao.getDaoObj();
 
-    private StudentInfoManagementImpl() {}
+    private SqlSessionFactory sqlSessionFactory;
+
+    private StudentInfoManagementImpl() {
+        // 加载配置文件
+        String resource = "mybatisConfig.xml";
+        try {
+            InputStream inputStream = Resources.getResourceAsStream(resource);
+            // 根据mybatis的配置创建SqlSessionFactory
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        } catch (IOException e) {
+            System.err.println("加载MyBatis配置文件失败");
+        }
+    }
 
     private static StudentInfoManagementImpl management;
 
@@ -23,7 +43,20 @@ public class StudentInfoManagementImpl implements StudentInfoManagement {
 
     @Override
     public List<StudentInfo> getAllStudentInfo() {
-        return dao.queryAllStudentInfo();
+        // 使用JDBC普通方法
+        // return dao.queryAllStudentInfo();
+
+        // 使用MyBatis
+        SqlSession session = sqlSessionFactory.openSession();
+        StudentInfoMapper mapper = session.getMapper(StudentInfoMapper.class);
+
+        // 执行查询
+        StudentInfoExample example = new StudentInfoExample();
+        StudentInfoExample.Criteria criteria = example.createCriteria();
+        List<StudentInfo> list = mapper.selectByExample(example);
+
+        session.close();
+        return list;
     }
 
 }
